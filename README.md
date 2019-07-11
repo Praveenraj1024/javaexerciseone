0a55455fb603f1b13a735834f1d061b58afcd289
praveen <praveenrajjoth@gmail.com>
2019-07-11 06:58:15 +0530

    problem2 completed

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ Input: 12345
 Output: 12345 is not palindrome
 Input: 12345654321
 Output : 12345654321 is palindrome and sum of even numbers is less than 25
+
+
+2. Write a program which accepts an integer number as input from the user and perform the
+following conditional checks:
+a. Print Tom if number is odd and exists between 20 to 30
+b. Print Jerry, if number is even and exists between 20 and 30
--- src/main/java/com/stackroute/practiceexercise1/Problem2.java
+++ src/main/java/com/stackroute/practiceexercise1/Problem2.java
@@ -1,9 +1,30 @@
 package com.stackroute.practiceexercise1;
 
+import java.util.Scanner;
+
 public class Problem2 {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int inputNum = scanner.nextInt();
+        if (isBetween20And30(inputNum)){
+            if (inputNum % 2 == 0){
+                System.out.println("Jerry");
+            }
+            else {
+                System.out.println("Tom");
+            }
+        }
+        else {
+            System.out.println("Not exist between 20 and 30");
+        }
+    }
 
-	// write your code here
+    private static boolean isBetween20And30(int num){
+        boolean boo = false;
+        if ( (num >= 20) && (num <= 30) ){
+            boo = true;
+        }
+        return boo;
     }
 }
