7ea17665cd9a60e674db77cd35e578bc3cb1e2d8
praveen <praveenrajjoth@gmail.com>
2019-07-11 12:31:49 +0530

    problem5 completed

--- src/main/java/com/stackroute/practiceexercise1/Problem5.java
+++ src/main/java/com/stackroute/practiceexercise1/Problem5.java
@@ -6,8 +6,30 @@ public class Problem5 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         String inputStringNumbers = scanner.nextLine();
-//        integerOrNot(inputStringNumbers);
+        String[] ar = inputStringNumbers.split(" ");
+        sumOfNum(ar);
     }
 
+    private static void sumOfNum(String[] strAr){
+        boolean boo = true;
+        int sum = 0;
+        for (String str: strAr) {
+            for (int i = 0; i < str.length(); i++){
+                if ( !(Character.isDigit(str.charAt(i))) ){
+                    boo = false;
+                }
+
+            }
+        }
+        if (boo){
+            for (String str: strAr){
+                sum += Integer.parseInt(str);
+            }
+            System.out.println(sum);
+        }
+        else {
+            System.out.println("You entered a non integer value");
+        }
+    }
 
 }
